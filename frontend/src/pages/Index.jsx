d5f7a6527838410e7b4a296f22bd3104ccb0a04f
nsuresh062@gmail.com <nsuresh062@gmail.com>
2024-11-19 19:52:59 +0530

    Canva-SignIn and Signup dialog

--- frontend/src/pages/Index.jsx
+++ frontend/src/pages/Index.jsx
@@ -1,22 +1,232 @@
-import React from 'react';
-
+import { set } from 'mongoose';
+import React , {useState}from 'react';
+import {RxCross2} from 'react-icons/rx';
+import { BiLogoGmail } from "react-icons/bi";
+import { FaFacebook } from "react-icons/fa";
 
 const Index = () => {
-  return (
-    <div className='bg-[#18191b] min-h-screen w-full'>
-        <div className='bg-[#212223] shadow-md'>
-            <div className='w-[93%] m-auto py-3'>
-                <div className='flex justify-between items-center'>
-                        <div className='w-[80px] h-[48px]'>
-                            <img 
-                             className='w-full h-full'src='https://static.canva.com/web/images/8439b51bb7a19f6e65ce1064bc37c197.svg' alt='canva' />
-                        </div>
-                </div>
 
-            </div>
+  const [type, setType] = useState('')
+  const [show, setShow] = useState(false)
+  const [state, setState] = useState({
+    name: '',
+    email: '',
+    password: ''
+  })
+
+  /**
+   * Handles input changes in the form. Updates the state with the new input
+   * value. This function is called whenever an input changes.
+   * @param {React.ChangeEvent<HTMLInputElement>} e - The event object from the input change event.
+   * @returns {void}
+   */
+  const inputHandle = (e) => {
+
+    setState({
+        ...state,
+        [e.target.name]: e.target.value
+    })
+
+  }
+
+console.log(state)
+  return (
+    <div className="bg-[#18191b] min-h-screen w-full">
+      <div
+        className={`w-screen ${
+          show ? "visible opacity-100" : "invisible opacity-30"
+        } transition-all duration-500 h-screen fixed bg-[#252627ad] flex justify-center items-center`}
+      >
+        <div className="w-[350px] bg-[#323335] m-auto px-6 py-4 rounded-md relative">
+          <div className="absolute right-4 top-4 text-xl cursor-pointer text-white">
+            <RxCross2 onClick={() => setShow(false)} />
+          </div>
+          <h2 className="text-white pb-4 text-center text-xl">
+            Login and Sign up in seconds
+          </h2>
+          {type === "signin" && (
+            <form>
+              <div className="flex flex-col gap-3 mb-3 text-white">
+                <label htmlFor="email">Email</label>
+                <input
+                  onChange={inputHandle}
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="email"
+                  value={state.email}
+                  className="px-3 py-2 rounded-md border outline-none border-[#5c5c5e] focus:border-purple-500 bg-transparent"
+                />
+              </div>
+
+              <div className="flex flex-col gap-3 mb-3 text-white">
+                <label htmlFor="password">Password</label>
+                <input
+                  onChange={inputHandle}
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="password"
+                  value={state.password}
+                  className="px-3 py-2 rounded-md border outline-none border-[#5c5c5e] focus:border-purple-500 bg-transparent"
+                />
+              </div>
+
+              <div>
+                <button className="px-3 py-2 rounded-md bg-purple-500 w-full outline-none hover:bg-purple-600 text-white">
+                  Signin
+                </button>
+              </div>
+
+              <div className="flex py-4 justify-between items-center px-3">
+                <div className="w-[45%] h-[1px] bg-slate-500 "></div>
+                <div className="w-[5%] text-center flex pb-1 px-1 text-white">
+                  Or
+                </div>
+                <div className="w-[45%] h-[1px] bg-slate-500 "></div>
+              </div>
+
+              <div className="pb-4">
+                <button className="px-3 py-2 flex justify-center items-center rounded-md bg-red-500 w-full outline-none hover:bg-red-600 text-white">
+                  <span>
+                    <BiLogoGmail />
+                  </span>
+                  <span>Login in with Google </span>
+                </button>
+              </div>
+              <div className="pb-4">
+                <button className="px-3 py-2 flex justify-center rounded-md bg-blue-500 w-full outline-none hover:bg-blue-600 text-white">
+                  <span>
+                    <FaFacebook />
+                  </span>
+                  <span> Login in with Facebook </span>
+                </button>
+              </div>
+            </form>
+          )}
+
+          {type === "signup" && (
+            <form>
+              <div className="flex flex-col gap-3 mb-3 text-white">
+                <label htmlFor="name">Name</label>
+                <input
+                  onChange={inputHandle}
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder="name"
+                  value={state.name}
+                  className="px-3 py-2 rounded-md border outline-none border-[#5c5c5e] focus:border-purple-500 bg-transparent"
+                />
+              </div>
+              <div className="flex flex-col gap-3 mb-3 text-white">
+                <label htmlFor="email">Email</label>
+                <input
+                  onChange={inputHandle}
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="email"
+                  value={state.email}
+                  className="px-3 py-2 rounded-md border outline-none border-[#5c5c5e] focus:border-purple-500 bg-transparent"
+                />
+              </div>
+
+              <div className="flex flex-col gap-3 mb-3 text-white">
+                <label htmlFor="password">Password</label>
+                <input
+                  onChange={inputHandle}
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="password"
+                  value={state.password}
+                  className="px-3 py-2 rounded-md border outline-none border-[#5c5c5e] focus:border-purple-500 bg-transparent"
+                />
+              </div>
+
+              <div>
+                <button className="px-3 py-2 rounded-md bg-purple-500 w-full outline-none hover:bg-purple-600 text-white">
+                  Sign Up
+                </button>
+              </div>
+
+              <div className="flex py-4 justify-between items-center px-3">
+                <div className="w-[45%] h-[1px] bg-slate-500 "></div>
+                <div className="w-[5%] text-center flex pb-1 px-1 text-white">
+                  Or
+                </div>
+                <div className="w-[45%] h-[1px] bg-slate-500 "></div>
+              </div>
+
+              <div className="pb-4">
+                <button className="px-3 py-2 flex justify-center items-center rounded-md bg-red-500 w-full outline-none hover:bg-red-600 text-white">
+                  <span>
+                    <BiLogoGmail />
+                  </span>
+                  <span>Login in with Google </span>
+                </button>
+              </div>
+              <div className="pb-4">
+                <button className="px-3 py-2 flex justify-center rounded-md bg-blue-500 w-full outline-none hover:bg-blue-600 text-white">
+                  <span>
+                    <FaFacebook />
+                  </span>
+                  <span> Login in with Facebook </span>
+                </button>
+              </div>
+            </form>
+          )}
         </div>
+      </div>
+
+      <div className="bg-[#212223] shadow-md">
+        <div className="w-[93%] m-auto py-3">
+          <div className="flex justify-between items-center">
+            <div className="w-[80px] h-[48px]">
+              <img
+                className="w-full h-full"
+                src="https://static.canva.com/web/images/8439b51bb7a19f6e65ce1064bc37c197.svg"
+                alt="canva"
+              />
+            </div>
+            <div className="flex gap-4">
+              <button
+                onClick={() => {setType('signin'); setShow(true)}}
+                className="py-2 w-[80px] text-center bg-teal-700 transition-all hover:bg-teal-500 rounded-[5px] font-medium"
+              >
+                SignIn
+              </button>
+              <button
+                onClick={() => {setType('signup'); setShow(true)}}
+                className="py-2 w-[80px] text-center bg-purple-700 transition-all hover:bg-purple-500 rounded-[5px] font-medium"
+              >
+                SignUp
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full h-full justify-center items-center p-4">
+        <div className="py-[170px] flex justify-center items-center flex-col gap-6">
+          <h2 className="text-5xl text-[#c7c5c5]">
+            {" "}
+            What you will design today?{" "}
+          </h2>
+          <span className="text-[#aca9a9] text-2xl font-medium">
+            Canva makes it easy to create and share professional designs.
+          </span>
+          <button
+            onClick={() => {setType('signup'); setShow(true)}}
+            className="py-2 w-[200px] text-center bg-purple-700 transition-all hover:bg-purple-500 rounded-[5px] font-medium text-white"
+          >
+            SignUp for Free
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Index;
